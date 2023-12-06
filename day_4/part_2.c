cb5e7c9f1ff68583971cd5d3ce3d3666ae2bb3dc
Mark Albrand <mark.albrand5@gmail.com>
2023-12-06 14:45:42 -0600

    part 2

---
+++ day_4/part_2.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_LINE_LENGTH 1000
+#define CARDS 6
+
+int main() {
+  printf("Hello world\n");
+
+  FILE *fp;
+
+  fp = fopen("sample.txt", "r");
+
+  if (fp == NULL) {
+    printf("Error opening file\n");
+    return 1;
+  }
+
+  char *lines[1000];
+  int line_count = 0;
+  char line[MAX_LINE_LENGTH];
+
+  while(fgets(line, MAX_LINE_LENGTH, fp) != NULL) {
+    line[strcspn(line, "\n")] = '\0';
+
+    lines[line_count] = strdup(line);
+
+    line_count++;
+  }
+
+  int total_cards = 0;
+  int cards[line_count];
+
+  for (int i = 0; i < line_count; i++){
+    cards[i] = 1;
+  }
+
+  for (int i = 0; i < line_count; i++) {
+    printf("i: %d cards[i]: %d", i, cards[i]);
+    int current_cards = cards[i];
+    for (int h = 0; h < current_cards; h++){
+
+      char *current_line = lines[i];
+      char *tkn = strtok(current_line, ":");  // Card N
+
+      int winning[100] = {-1}; // Todos inician en -1
+      bool finding = false;
+      int next_card = i;
+    
+      int counter = 0;
+      int subtotal = 0;
+
+      printf("\t%s\n", tkn);
+
+      tkn = strtok(NULL, ":");
+    
+      char *nums = strtok(tkn, " ");
+      while(nums != NULL){
+        if (strcmp(nums, "|") == 0){
+          finding = true;
+          nums = strtok(NULL, " ");
+          continue;
+        }
+
+        if (finding){
+          //printf("Finding: '%s'\n", nums);
+          int x = atoi(nums);
+
+          for (int j = 0; j < counter; j++) {
+            if (winning[j] == x){
+              // Si hay un número ganador, se obtiene una carta nueva
+              next_card++;
+              cards[next_card] += 1;
+              printf("%d Card %d: %d\n", winning[j], (next_card+1), cards[next_card]);
+            }  
+          }
+  
+        } else { // Add to wining
+          //printf("'%s'\n", nums);  // Cada número después de 'Card N:'
+          int n = atoi(nums);
+          winning[counter] = n;
+          counter++;
+        }
+        nums = strtok(NULL, " ");
+      } 
+    }
+  }
+
+  printf("\nTOTAL: %i\n", total_cards);
+
+  for (int i = 0; i < line_count; i++) {
+    printf("Card %i: %i\n", (i+1), cards[i]);
+  }
+
+  return 0;
+}
