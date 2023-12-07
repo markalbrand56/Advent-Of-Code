17d6b946d1dee4071a62b7bcde16e8d0cc5ccb9d
Mark Albrand <mark.albrand5@gmail.com>
2023-12-06 18:24:31 -0600

    day 5

--- day_5/part_1.cpp
+++ day_5/part_1.cpp
@@ -6,10 +6,17 @@
 #include <vector>
 using namespace std;
 
+map<int, int> build_map(vector<string> &vec) {
+  map<int, int> range;
+
+  // DST | SRC | RANGE
+
+  return range;
+}
+
 int main() {
   cout << "Hello World" << endl;
 
-  // DST | SRC | RANGE
   
   ifstream file("sample.txt");
 
@@ -36,57 +43,79 @@ int main() {
 
   // División de líneas
   string seeds = lineas[0];  // lineas[1]: ""
-  int i = 2;
+  vector<string> seed_soil;
+  int i = 3;
 
 
   // Seed to soil
   cout << "SEED TO SOIL" << endl;
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    seed_soil.push_back(lineas[i]);
   }
 
   cout << "SOILD TO FERTILIZER" << endl;
-  i++;
+  vector<string> soil_fertilizer;
+  i+=2;
 
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    soil_fertilizer.push_back(lineas[i]);
   }
 
   cout << "FERTILIZER TO WATER" << endl;
-  i++;
+  vector<string> fertilizer_water;
+  i+=2;
 
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    fertilizer_water.push_back(lineas[i]);
   }
 
   cout << "WATER TO LIGHT" << endl;
-  i++;
+  vector<string> water_light;
+  i+=2;
 
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    water_light.push_back(lineas[i]);
   }
 
   cout << "LIGHT TO TEMP" << endl;
-  i++;
+  vector<string> light_temp;
+  i+=2;
 
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    light_temp.push_back(lineas[i]);
   }
 
   cout << "TEMP TO HUMIDITY" << endl;
-  i++;
+  vector<string> temp_humidity;
+  i+=2;
 
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    temp_humidity.push_back(lineas[i]);
   }
 
   cout << "HUMIDITY TO LOCATION" << endl;
-  i++;
+  vector<string> humidity_location;
+  i+=2;
 
   for(; !lineas[i].empty(); i++){
     cout << lineas[i] << endl;
+    humidity_location.push_back(lineas[i]);
   }
 
+  // Construir maps
+  map<int, int> r_seed_soil = build_map(seed_soil);
+  map<int, int> r_soil_fertilizer = build_map(soil_fertilizer);
+  map<int, int> r_fertilizer_water = build_map(fertilizer_water);
+  map<int, int> r_water_light = build_map(water_light);
+  map<int, int> r_light_temp = build_map(light_temp);
+  map<int, int> r_temp_humidity = build_map(temp_humidity);
+  map<int, int> r_humidity_location = build_map(humidity_location);
 
   return 0;
 }
