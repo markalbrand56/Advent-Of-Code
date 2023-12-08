cd0304f410a83ea8aa445be35d861785e2d85238
Mark Albrand <mark.albrand5@gmail.com>
2023-12-07 22:47:40 -0600

    day 7: type of hands

--- day_7/src/part_1.rs
+++ day_7/src/part_1.rs
@@ -1,10 +1,56 @@
+use std::collections::HashMap;
 use std::fs;
+
+#[derive(Debug)]
+enum HandType{
+    FiveOfKind,
+    FourOfKind,
+    FullHouse,
+    ThreeOfKind,
+    TwoPair,
+    OnePair,
+    High,
+    None,
+}
 
 struct Hand {
     hand: String,
     bet: i32,
     rank: i32,
-    type_hand: i32,
+    type_hand: HandType,
+}
+
+impl Hand {
+    fn determine_hand_type(&mut self) {
+        let mut counts = HashMap::new();
+
+        for c in self.hand.chars() {
+            let counter = counts.entry(c).or_insert(0); // Si no encuentra ingresa 0
+            *counter +=1;
+        }
+
+        let max_count = counts.values().cloned().max().unwrap();
+
+        match max_count {
+            5 => self.type_hand = HandType::FiveOfKind,
+            4 => self.type_hand = HandType::FourOfKind,
+            3 => {
+                if counts.len() == 2 {
+                    self.type_hand = HandType::FullHouse;
+                } else {
+                    self.type_hand = HandType::ThreeOfKind;
+                }
+            }
+            2 => {
+                if counts.len() == 3 {
+                    self.type_hand = HandType::TwoPair;
+                } else {
+                    self.type_hand = HandType::OnePair;
+                }
+            }
+            _ => self.type_hand = HandType::High,
+        }
+    }
 }
 
 pub fn part_1(){
@@ -24,18 +70,18 @@ pub fn part_1(){
             let key = words[0];
             let value = words[1].parse::<i32>().unwrap();
 
-            let h = Hand{
+            let mut h = Hand{
                 hand: String::from(key),
                 bet: value,
                 rank: 0,
-                type_hand: 0,
+                type_hand: HandType::None,
             };
-
+            h.determine_hand_type();
             hands.push(h);
         }
     }
 
     for hand in &hands {
-        println!("{} {}", hand.hand, hand.bet)
+        println!("{} {} {:?}", hand.hand, hand.bet, hand.type_hand);
     }
 }
