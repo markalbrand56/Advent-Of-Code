835a142167cc1938bfd18b2d6dcefbf1cf9cc8c5
Mark Albrand <mark.albrand5@gmail.com>
2023-12-10 20:32:39 -0600

    day 9 p2

--- day_9/main.go
+++ day_9/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "day_9/part_1"
+import (
+	"day_9/part_1"
+	"day_9/part_2"
+)
 
 func main() {
 	part_1.Part1()
+	part_2.Part2()
 }
---
+++ day_9/part_2/part_2.go
@@ -0,0 +1,72 @@
+package part_2
+
+import (
+	"day_9/utils"
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func Part2() {
+	fmt.Println("Part 2")
+	var fileName = "sample.txt"
+
+	readings, err := utils.ReadFile(fileName)
+
+	if err != nil {
+		panic("Error opening file. " + err.Error())
+	}
+
+	var extrapolations [][][]int
+	for _, r := range readings {
+		vals := strings.Split(r, " ")
+		var nums []int
+		for _, v := range vals {
+			n, _ := strconv.Atoi(v)
+			nums = append(nums, n)
+		}
+		var extrapolation [][]int
+		extrapolation = append(extrapolation, nums)
+		extrapolations = append(extrapolations, extrapolation)
+	}
+
+	for i := range extrapolations {
+		zeroes := false
+		j := 0
+		for zeroes != true {
+			var diffs []int // diferencias entre los valores de una linea
+			line := extrapolations[i][j]
+			for k := 0; k < len(line)-1; k++ {
+				//fmt.Printf("%d-%d: %d\n", line[k], line[k+1], line[k+1]-line[k])
+				diffs = append(diffs, line[k+1]-line[k])
+			}
+			extrapolations[i] = append(extrapolations[i], diffs)
+			if slices.Min(diffs) == 0 && slices.Max(diffs) == 0 {
+				zeroes = true
+			}
+			j++
+		}
+		//fmt.Println(extrapolations[i])
+	}
+
+	for i := range extrapolations {
+		slices.Reverse(extrapolations[i])
+
+		for j := 0; j < len(extrapolations[i])-1; j++ {
+			line := extrapolations[i][j]
+			k := len(extrapolations[i][j+1]) - 1
+			v := line[len(line)-1]
+
+			extrapolations[i][j+1] = append(extrapolations[i][j+1], extrapolations[i][j+1][k]+v)
+		}
+	}
+
+	var resultado int
+	for _, ex := range extrapolations {
+		line := ex[len(ex)-1]
+		resultado += line[len(line)-1]
+	}
+
+	fmt.Printf("Resultado: %d\n", resultado)
+}
