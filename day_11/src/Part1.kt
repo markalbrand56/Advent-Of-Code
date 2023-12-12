d48167def39796e54728a6052385f7eb1743e5be
Mark Albrand <mark.albrand5@gmail.com>
2023-12-11 19:04:20 -0600

    day 11

--- day_11/src/Part1.kt
+++ day_11/src/Part1.kt
@@ -1,3 +1,44 @@
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    println("Part 1")
+
+    val lines = File("src/sample.txt").readLines()
+    val image: MutableList<MutableList<Char>> = ArrayList()
+
+    lines.forEach { line ->
+        println(line)
+
+        val l: MutableList<Char> = ArrayList()
+        line.forEach {char ->
+            l.add(char)
+        }
+        image.add(l)
+    }
+
+
+    val h = image.size - 1  // Rows
+    val w = image[0].size - 1  // Columns
+    val imageExpanded: MutableList<MutableList<Char>> = ArrayList()
+
+    // Revisar filas
+    for (i in 0..h){
+        var hasGalaxy = false
+        for (j in 0..w){
+            val c = image[i][j]
+            if( c != '.'){
+                hasGalaxy = true
+            }
+            println(c)
+        }
+        val fila = image[i]
+        if (!hasGalaxy){ // Si la fila no tenía una galaxia
+            imageExpanded.add(fila)
+            imageExpanded.add(fila)
+        } else{
+            imageExpanded.add(fila)
+        }
+
+    }
+    println()
 }
