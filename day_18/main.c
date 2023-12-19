4e7d1ab409e82abe0b3e083fc95eedd6dd322613
Mark Albrand <mark.albrand5@gmail.com>
2023-12-18 22:01:53 -0600

    day 18 part 1

--- day_18/main.c
+++ day_18/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "part_1.h"
 
 int main() {
 	printf("Hello, World!\n");
+	part_1();
 	return 0;
 }
---
+++ day_18/part_1.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void part_1() {
+	printf("Part 1\n");
+
+	FILE *fp;
+
+	fp = fopen("sample.txt", "r");
+
+	if (fp == NULL) {
+		printf("Error opening file\n");
+		return;
+	}
+
+	char *lines[1000];
+	int length = 0;
+	char line[1000];
+
+	while(fgets(line, 1000, fp) != NULL) {
+		line[strcspn(line, "\n")] = '\0';
+
+		lines[length] = strdup(line);
+
+		length++;
+	}
+
+	fclose(fp);
+
+	for (int i = 0; i < length; i++) {
+		char *l = lines[i];
+
+		printf("%s\n", l);
+	}
+}
